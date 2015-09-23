76d44e4d58dc9fa980e4140ecd65e92030fbdfd8
Walid Sultan <walidalysultan@gmail.com>
2015-09-23 03:21:45 -0500

    Added "Add Interest" feature

--- ASD-Project/src/BankApp/ui/BankHeaderPane.java
+++ ASD-Project/src/BankApp/ui/BankHeaderPane.java
@@ -3,15 +3,19 @@ package BankApp.ui;
 import java.io.IOException;
 
 import BankApp.controllers.AccountController;
+import BankApp.models.BankAccount;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
 import javafx.stage.StageStyle;
 import framework.ui.HeaderPane;
+import framework.Account;
+import framework.IAccount;
 import framework.ICustomer;
 import framework.Organization;
 import framework.Person;
+import framework.State;
 public class BankHeaderPane extends HeaderPane {
 
 	@FXML
@@ -24,6 +28,15 @@ public class BankHeaderPane extends HeaderPane {
 		showAccountDialog(new Organization(),"Add Company Account");
 	}
 
+	@FXML
+	private void addInterest()  {
+		for(IAccount account: State.getInstance().getAccounts()){
+			account.setAmount(account.getAmount()+account.getAmount()*.1);
+		}
+		BankContentPane.getInstance().update();
+	}
+	
+	
 	private void showAccountDialog(ICustomer customer,String title) throws IOException {
 		Stage dialog = new Stage();
 		dialog.initStyle(StageStyle.UTILITY);
--- ASD-Project/src/framework/IAccount.java
+++ ASD-Project/src/framework/IAccount.java
@@ -1,5 +1,11 @@
 package framework;
 
 public interface IAccount {
+	ICustomer getCustomer();
 
+	double getAmount();
+
+	void setAmount(double amount);
+
+	void addEntry(IEntry entry);
 }
